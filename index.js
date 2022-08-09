568309c36bad73fe0173493e6adbf3ab92246458
khadija.dahir <khadija.dahir@student.moringaschool.com>
2022-08-09 17:38:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+
+function countDown(a){
+    while (a > 0){
+        console.log(a);
+        a--;
+    }
+    console.log(a)
+}
